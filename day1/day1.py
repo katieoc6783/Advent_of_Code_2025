aa08b61b27bd5190062759f814092ff871357180
Katie <119692854+katieoc6783@users.noreply.github.com>
2026-08-12 16:18:16 +0100

    day1 part 1

---
+++ day1/day1.py
@@ -0,0 +1,25 @@
+file = open("day1Input.txt", "r")
+line = file.readline()
+num = 50
+passw = 0
+while line:
+    line = line.strip()
+    lineNum = int(line[1:])
+    if line[:1] == "R":
+    #we increase
+        num = num + lineNum 
+        while num > 99:
+            num = num - 100
+    else:
+    #we decrease
+        num = num - lineNum
+        while num < 0:
+            num = 100 + num
+
+    if num == 0:
+        passw += 1
+    line = file.readline()
+
+print(passw)
+
+file.close()
